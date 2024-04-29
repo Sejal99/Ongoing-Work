0606625f7acbdd466ec7f4a5590e37307c1b1d3f
sejal99 <sejal8974@gmail.com>
2024-04-29 10:39:30 +0530

    server side search done

--- server-side/routes/product.js
+++ server-side/routes/product.js
@@ -5,7 +5,22 @@ const productRouter=express.Router();
 
 productRouter.get('/', async (req, res) => {
     try {
-        const products = await Product.find();
+        const searchTerm = req.query.search;
+console.log(searchTerm);
+        // Define a base query to find products
+        let query = {};
+
+        // If a search term is provided, modify the query to search for products
+        if (searchTerm) {
+            query = {
+                $or: [
+                    { name: { $regex: searchTerm, $options: 'i' } }, 
+                    { description: { $regex: searchTerm, $options: 'i' } } ,
+                    { category: { $regex: searchTerm, $options: 'i' } } 
+                ]
+            };
+        }
+        const products = await Product.find(query);
         res.json(products);
     } catch (error) {
         console.error('Error fetching products:', error);

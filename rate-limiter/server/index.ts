0e341a167a311f13ac6b0982cc0b123c0b64d10e
sejal99 <sejal8974@gmail.com>
2024-05-04 22:30:07 +0530

    new commit

---
+++ rate-limiter/server/index.ts
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const userRoutes = require('./routes/userRoutes');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.json());
+
+// // Route for signup API
+// app.use('/api/user', userRoutes);
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });

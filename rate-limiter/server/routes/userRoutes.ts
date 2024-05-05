d5e1784f8f7a5bdbe957b2c40c9eeaba81cbe241
sejal99 <sejal8974@gmail.com>
2024-05-05 22:56:17 +0530

    redis connected

--- rate-limiter/server/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Route for signup
-router.post('/signup', userController.signup);
-
-module.exports = router;

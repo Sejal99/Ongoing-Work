939e8be77581431d40904d7eb080f7fe74e42848
sejal99 <sejal8974@gmail.com>
2024-05-07 17:48:04 +0530

    new commit

---
+++ rate-limiter/index.js
@@ -0,0 +1,31 @@
+const express=  require("express")
+const app= express()
+const path= require("path")
+const staticRoute= require("./routes/staticRouter")
+const redis= require('redis')
+const client= redis.createClient()
+app.use(express.json())
+
+app.set('view engine', "ejs")
+app.set("views", path.resolve('./views')) //app.set("views", path.join(__dirname, 'views'));
+
+app.use('/', staticRoute )
+
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
+
+client.on('error', function (err) {
+    console.error('Redis error:', err);
+});
+
+// app.get('/', (req,res)=> {
+//     res.render("home")
+// })
+
+
+
+app.listen(3000, ()=> console.log('Listening on port 3000'))
+
+
+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

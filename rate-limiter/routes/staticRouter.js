760b1557a5b73eee993411a4305de040edcf958b
sejal99 <sejal8974@gmail.com>
2024-06-07 22:45:23 +0530

    new commit

--- rate-limiter/routes/staticRouter.js
+++ rate-limiter/routes/staticRouter.js
@@ -1,24 +1,24 @@
-const express= require("express");
-const limiter = require("../middleware/limiter");
+// const express= require("express");
+// const limiter = require("../middleware/limiter");
 
-const router= express.Router()
+// const router= express.Router()
 
-let url1Items=[]
-router.get('/', (req,res)=> {
-    res.render('home',{
-        url1data: url1Items
-    })
-});
+// let url1Items=[]
+// router.get('/', (req,res)=> {
+//     res.render('home',{
+//         url1data: url1Items
+//     })
+// });
 
-router.get('/url1', limiter(60,10),  async(req,res)=> {
-    console.log('generation');
-    try{
-        let url1Output =""  //newItem= req.body.name
-         url1Items.push(url1Output)
-        res.redirect("/")
-    }catch(err){
-        res.status(403).json({message:err})
-    }
-})
+// router.get('/url1', limiter(60,10),  async(req,res)=> {
+//     console.log('generation');
+//     try{
+//         let url1Output =""  //newItem= req.body.name
+//          url1Items.push(url1Output)
+//         res.redirect("/")
+//     }catch(err){
+//         res.status(403).json({message:err})
+//     }
+// })
 
-module.exports= router
+// module.exports= router

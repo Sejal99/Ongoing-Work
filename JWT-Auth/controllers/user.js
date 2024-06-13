aa7feb2bbc4a50fb9fb3e3211ee26e34ca6c9240
sejal99 <sejal8974@gmail.com>
2024-06-13 11:22:58 +0530

    jwt auth

--- JWT-Auth/controllers/user.js
+++ JWT-Auth/controllers/user.js
@@ -1,3 +1,16 @@
-export default handleLogin=async(req,res)=>{
+import user from "../models/user.js";
+
+const handleLogin=async(req,res)=>{
+const {email,password}=req.body;
+try {
+    const User=await user.findOne({email,password}); 
+} catch (error) {
+    
+}
+
 
 }
+
+
+
+export default handleLogin;
--- JWT-Auth/routes/user.js
+++ JWT-Auth/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express"
-import handleLogin from '../controllers/user'
+import handleLogin from '../controllers/user.js'
 const router=express.Router();
 
 router.post('/login',handleLogin);

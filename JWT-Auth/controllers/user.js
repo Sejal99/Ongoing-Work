edb72e0b4e2990bdc773a1d2ecdeb4000ff052be
sejal99 <sejal8974@gmail.com>
2024-06-13 12:01:57 +0530

    jwt auth

--- JWT-Auth/controllers/user.js
+++ JWT-Auth/controllers/user.js
@@ -3,35 +3,31 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = await user.create({ email, password: hashedPassword });
-      res.status(201).json(newUser);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await user.create({ email, password: hashedPassword });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const handleLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const users = await user.findOne({ email });
+    if (!users) {
+      return res.status(401).json({ message: "User not found" });
     }
-  };
-  
- const handleLogin = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const users = await user.findOne({ email });
-      if (!users) {
-        return res.status(401).json({ message: "User not found" });
-      }
-      const validPassword = await bcrypt.compare(password, users.password);
-      if (!validPassword) {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-      const token = jwt.sign({ userId: users._id }, process.env.JWT_SECRET, {
-        expiresIn: "1h",
-      });
-      //setting cookie in response
-      res.cookie("jwt", token, { httpOnly: true, maxAge: 3600000 });
-      res.json({ token });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  };
-  export default handleLogin;
+
+    const token = jwt.sign({ userId: users._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+export default handleLogin;

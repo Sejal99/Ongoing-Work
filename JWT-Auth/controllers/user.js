fcac52ae83bb2309e50df2ec7423cd7efe1624c5
sejal99 <sejal8974@gmail.com>
2024-06-13 12:00:38 +0530

    jwt auth

--- JWT-Auth/controllers/user.js
+++ JWT-Auth/controllers/user.js
@@ -1,16 +1,37 @@
 import user from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const handleLogin=async(req,res)=>{
-const {email,password}=req.body;
-try {
-    const User=await user.findOne({email,password}); 
-} catch (error) {
-    
-}
-
-
-}
-
-
-
-export default handleLogin;
+export const signup = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await user.create({ email, password: hashedPassword });
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  
+ const handleLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const users = await user.findOne({ email });
+      if (!users) {
+        return res.status(401).json({ message: "User not found" });
+      }
+      const validPassword = await bcrypt.compare(password, users.password);
+      if (!validPassword) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+      const token = jwt.sign({ userId: users._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      //setting cookie in response
+      res.cookie("jwt", token, { httpOnly: true, maxAge: 3600000 });
+      res.json({ token });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  export default handleLogin;
--- JWT-Auth/routes/user.js
+++ JWT-Auth/routes/user.js
@@ -1,7 +1,7 @@
 import express from "express"
-import handleLogin from '../controllers/user.js'
+import handleLogin, { signup } from '../controllers/user.js'
 const router=express.Router();
-
+router.post('/signup',signup)
 router.post('/login',handleLogin);
 
 export default router

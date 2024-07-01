d16ebb71e002a201be0ecc0e8d82890583469844
sejal99 <sejal8974@gmail.com>
2024-07-01 13:13:34 +0530

    aggreagtion pipeline

--- mongodb_aggregation/server/data.tsx
+++ mongodb_aggregation/server/data.tsx
@@ -12,6 +12,7 @@ db.orders.aggregate([
         }
       }
     ])
+
 ANS:
 
 { "_id" : "banana", "totalQuantity" : 7, "totalPrice" : 7 }
@@ -21,7 +22,21 @@ ANS:
 
 
 
+Example 2: Filtering and Sorting
+Assume you want to find the top 3 customers by their total purchase amount from a customers collection:
 
+db.customers.aggregate([
+  {
+    $group: {
+      _id: "$customerId",
+      totalPurchaseAmount: { $sum: "$purchaseAmount" }
+    }
+  },
+  { $sort: { totalPurchaseAmount: -1 } },
+  { $limit: 3 }
+])
+This pipeline groups customers by customerId, calculates their totalPurchaseAmount,
+ sorts them in descending order by totalPurchaseAmount, and limits the output to the top 3 customers
 
 
     */

14a4816a278f32c731da64571556ee8919bc8bd0
sejal99 <sejal8974@gmail.com>
2024-08-16 23:14:22 +0530

    new commit

--- webhooks-Stripe/index.js
+++ webhooks-Stripe/index.js
@@ -1,33 +1,48 @@
-const express = require("express")
+const express = require("express");
 
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
-const stripe=require("stripe")("sk_test_51PmHJrRwMOzblwjNXVpacPizJrBXeRNn8uP55zrYcooYm5I71O5zaDp497EYZHL8RGOTCcU458CHNjewOJtbS6V700zDYokJQW")
+const stripe = require("stripe")(
+  "sk_test_51PmHJrRwMOzblwjNXVpacPizJrBXeRNn8uP55zrYcooYm5I71O5zaDp497EYZHL8RGOTCcU458CHNjewOJtbS6V700zDYokJQW"
+);
 const app = express();
 
-const bodyparser=require("body-parser")
+const bodyparser = require("body-parser");
 
-
-app.post('/hooks',bodyparser.raw({type:'application/json'}),async(req,res)=>{
-    let signingsecret=process.env.SIGN_SECRET;
+app.post(
+  "/hooks",
+  bodyparser.raw({ type: "application/json" }),
+  async (req, res) => {
+    let signingsecret = process.env.SIGN_SECRET;
     console.log(signingsecret);
 
-    const payload=req.body;
-    const sig =req.headers['stripe-signature']
-
+    const payload = req.body;
+    const sig = req.headers["stripe-signature"];
 
     //matching these wenhook is from stripe
 
-    
+    let event;
 
-    
-})
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, signingsecret);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ success: false });
+      return;
+    }
 
+    //successfull
 
+    console.log(event.type);
+    console.log(event.data.object);
+    console.log(event.data.object.id);
 
+    res.json({
+      success: true,
+    });
+  }
+);
 
-
-app.listen(5000,()=>{
-    console.log('server listening on 5000');
-    
-})
+app.listen(5000, () => {
+  console.log("server listening on 5000");
+});

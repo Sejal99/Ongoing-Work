78679a56d42ac324bdacd0b56717b5b8d408d9eb
sejal99 <sejal8974@gmail.com>
2024-08-16 22:49:03 +0530

    new commit

---
+++ webhooks-Stripe/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const dotenv =require("dotenv");
+dotenv.config();
+const stripe=require("stripe")("sk_test_51PmHJrRwMOzblwjNXVpacPizJrBXeRNn8uP55zrYcooYm5I71O5zaDp497EYZHL8RGOTCcU458CHNjewOJtbS6V700zDYokJQW")
+const app = express();
+
+const bodyparser=require("body-parser")
+
+
+app.post('/hooks',async(req,res)=>{
+    let signingsecret=process.env.SIGN_SECRET;
+    console.log(signingsecret);
+    
+})
+
+
+
+app.listen(5000,()=>{
+    console.log('server listening on 5000');
+    
+})

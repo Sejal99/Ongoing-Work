1b65c12c0c5acab1f1f3a245bf84d7abcc3bd2de
sejal99 <sejal8974@gmail.com>
2024-08-17 12:56:49 +0530

    webhook creation

--- StripePayment/stripe-Invoicemail/index.js
+++ StripePayment/stripe-Invoicemail/index.js
@@ -12,6 +12,41 @@ app.get("/",(req,res)=>{
   res.send("hii");
   
 })
+
+//webhook
+
+let session=
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.async_payment_failed':
+      session = event.data.object;
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      break;
+    case 'checkout.session.async_payment_succeeded':
+      session = event.data.object;
+      // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
 app.listen(5000, () => {
     console.log("server listening on 5000");
   });
